8aa95b586a0d378065a55692c8a014ec7f9def84
teena22 <tggargteena18@gmail.com.com>
2017-03-20 01:20:09 +0530

    My first commit

---
+++ project/myart/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    ar_email = models.EmailField(max_length=70,blank=True)
+    pic= models.FileField(upload_to='documents/%Y/%m/%d')
